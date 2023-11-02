1079952a23acde2d22f3b61027574d0b03c4c20e
Bartłomiej Skorupski <bartmann001@gmail.com>
2023-11-02 02:58:24 +0100

    more presets, r-pentomino & bi-block puffer

---
+++ GameOfLifeWPF/Model/BoardFactory/RPentominoBoardFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeWPF.Model.BoardFactory
+{
+    public class RPentominoBoardFactory : StructureBoardFactory
+    {
+        protected override int[,] Structure => new int[,]
+        {
+            { 0,1,1 },
+            { 1,1,0 },
+            { 0,1,0 },
+        };
+        protected override int StructureWidth => 3;
+        protected override int StructureHeight => 3;
+        public RPentominoBoardFactory(int width, int height) : base(width, height) { }
+    }
+}
--- GameOfLifeWPF/Views/CreateGameView.xaml.cs
+++ GameOfLifeWPF/Views/CreateGameView.xaml.cs
@@ -31,13 +31,15 @@ namespace GameOfLifeWPF.Views
             BoardPresetItems = new List<BoardPresetItem>()
             {
                 new BoardPresetItem("Empty", typeof(EmptyBoardFactory)),
+                new BoardPresetItem("R-Pentomino", typeof(RPentominoBoardFactory), 3, 3, "DarkGreen"),
                 new BoardPresetItem("Acorn", typeof(AcornBoardFactory), 7, 3, "SaddleBrown"),
                 new BoardPresetItem("Glider", typeof(GliderBoardFactory), 3, 3, "MidnightBlue"),
                 new BoardPresetItem("Light-weight Spaceship", typeof(LWSSBoardFactory), 5, 4, "DarkGray"),
                 new BoardPresetItem("Middle-weight Spaceship", typeof(MWSSBoardFactory), 6, 5, "Gray"),
                 new BoardPresetItem("Heavy-weight Spaceship", typeof(HWSSBoardFactory), 7, 5, "DimGray"),
                 new BoardPresetItem("Pulsar", typeof(PulsarBoardFactory), 13, 13, "DarkMagenta"),
-                new BoardPresetItem("Glider Gun", typeof(GliderGunBoardFactory), 36, 9, "Maroon")
+                new BoardPresetItem("Glider Gun", typeof(GliderGunBoardFactory), 36, 9, "Maroon"),
+                new BoardPresetItem("Bi-Block Puffer", typeof(BiBlockPufferBoardFactory), 37, 29, "Black"),
             };
             MaxBoardSize = 999;
             DataContext = this;
